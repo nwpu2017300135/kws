81b57afff77eaae2d620ff13780eb942db3a90ec
zhangao <838966118@qq.com>
2020-05-07 10:01:28 +0800

    init

---
+++ input_data.py
@@ -0,0 +1,270 @@
+import fbank_reader
+import numpy as np
+import os
+def getname(example):
+    return example.split('/')[-1][:-6]
+class TestSet(object):
+    def __init__(self, exampls, labels, num_examples, fbank_end_frame):
+        self._exampls = exampls
+        self._labels = labels
+        self._index_in_epochs = 0
+        self.num_examples = num_examples
+        self.fbank_end_frame = fbank_end_frame
+
+    def next_batch(self, batch_size):
+        start = self._index_in_epochs
+
+        if start + batch_size > self.num_examples:
+            self._index_in_epochs = self.num_examples
+            end = self._index_in_epochs
+            return self._exampls[start:end], self._labels[start:end]
+        else:
+            self._index_in_epochs += batch_size
+            end = self._index_in_epochs
+            return self._exampls[start:end], self._labels[start:end]
+
+
+class TrainSet(object):
+    def __init__(self, examples_list, position_data):
+        self.examples_list = examples_list
+        self.position_data = position_data
+        self.fbank_position = 0
+        self.index_in_epochs = 0
+        self.example = []
+        self.labels = []
+        self.num_examples = 0
+
+    def read_train_set(self):
+        self.example = []
+        self.labels = []
+        self.num_examples = 0
+        step_length = 10
+        start = self.fbank_position % len(self.examples_list)
+        end = (self.fbank_position + step_length) % len(self.examples_list)
+        if start < end:
+            fbank_list = self.examples_list[start: end]
+            self.fbank_position += step_length
+
+        else:
+            fbank_list = self.examples_list[start: len(self.examples_list)]
+            self.fbank_position = 0
+            index = np.arange(len(self.examples_list))
+            np.random.shuffle(index)
+            self.examples_list = np.array(self.examples_list)[index]
+
+        for example in fbank_list:
+            if example == '':
+                continue
+            file_path = example.split(" ")[1]
+            if example.split('/').count("positive")>0:
+                start = self.position_data.find(getname(example))
+                end = self.position_data.find("positive", start + 1)
+                if end != -1:
+                    position_str = self.position_data[start + 15: end - 1]
+                else:
+                    position_str = self.position_data[start + 15: end]
+
+                # start and end position of "hello" & start and end position of "xiao gua"
+                keyword_position = position_str.split(" ")
+
+                file_path = example.split(" ")[1]
+
+                keyword_frame_position = []
+                for i in range(4):
+                    fbank = fbank_reader.HTKFeat_read(file_path).getall()
+                    length = fbank.shape[0]
+                    frame_position =(int(keyword_position[i])-240) // 160
+                    if frame_position >= length:
+                        frame_position = length - 1
+                    keyword_frame_position.append(frame_position)
+
+                #print (example)
+
+
+                for frame in range(keyword_frame_position[0], keyword_frame_position[1] + 1):
+                    self.example.append(
+                        frame_combine(frame, file_path, keyword_frame_position[0], keyword_frame_position[1]))
+                    self.labels.append('0')
+                    self.num_examples += 1
+                for frame in range(keyword_frame_position[2], keyword_frame_position[3] + 1):
+                    self.example.append(
+                        frame_combine(frame, file_path, keyword_frame_position[2], keyword_frame_position[3]))
+                    self.labels.append('1')
+                    self.num_examples += 1
+
+            else:
+                file_path =example.split(" ")[1]
+
+                fbank = fbank_reader.HTKFeat_read(file_path).getall()
+                frame_number = fbank.shape[0]
+
+                #print (example)
+
+
+
+
+
+
+
+
+                for frame in range(frame_number):
+                    self.example.append(frame_combine(frame, file_path, 0, frame_number - 1))
+                    self.labels.append('2')
+                    self.num_examples += 1
+#                    print(self.example.shape)
+    def next_batch(self, batch_size):
+        start = self.index_in_epochs
+
+        if start == 0:
+            self.read_train_set()
+            index0 = np.arange(self.num_examples)
+            np.random.shuffle(index0)
+            self.example = np.array(self.example)[index0]
+            self.labels = np.array(self.labels)[index0]
+
+        if start + batch_size > self.num_examples:
+            examples_rest_part = self.example[start: self.num_examples]
+            labels_rest_part = self.labels[start: self.num_examples]
+            self.index_in_epochs = 0
+            return examples_rest_part, labels_rest_part
+
+        else:
+            self.index_in_epochs += batch_size
+            end = self.index_in_epochs
+            return self.example[start:end], self.labels[start:end]
+
+
+def frame_combine(frame, file_path, start, end):
+    fbank = fbank_reader.HTKFeat_read(file_path).getall()
+
+    if end - start + 1 < 41:
+        if frame - start <= 30 and end - frame <= 10:
+            frame_to_combine = []
+            front_rest = 30 - (frame - start)
+            back_rest = 10 - (end - frame)
+            for i in range(front_rest):
+                frame_to_combine.append(fbank[start].tolist())
+            for i in range(start, end + 1):
+                frame_to_combine.append(fbank[i].tolist())
+            for i in range(back_rest):
+                frame_to_combine.append(fbank[end].tolist())
+
+        elif end - frame >= 10:
+            frame_to_combine = []
+            front_rest = 30 - (frame - start)
+            for i in range(front_rest):
+                frame_to_combine.append(fbank[start].tolist())
+            for i in range(start, frame+11):
+                frame_to_combine.append(fbank[i].tolist())
+
+        else:
+            frame_to_combine = []
+            back_rest = 10 - (end - frame)
+            for i in range(frame - 30, end + 1):
+                frame_to_combine.append(fbank[i].tolist())
+            for i in range(back_rest):
+                frame_to_combine.append(fbank[end].tolist())
+        combined = np.array(frame_to_combine).reshape(-1)
+
+    else:
+        if frame - start >= 30 and end - frame >= 10:
+            frame_to_combine = fbank[frame - 30: frame + 11]
+            combined = frame_to_combine.reshape(-1)
+
+        elif frame - start < 30:
+            frame_to_combine = fbank[start: start+41]
+            combined = frame_to_combine.reshape(-1)
+
+        else:
+            frame_to_combine = fbank[end - 40: end+1]
+            combined = frame_to_combine.reshape(-1)
+
+    return combined.tolist()
+
+
+def read_data_sets():
+    f = open("/home/disk2/internship_anytime/aslp_hotword_data/aslp_wake_up_word_data/positiveKeywordPosition.txt", "r")
+    position_data = f.read()
+    f.close()
+
+    f = open("/home/disk2/internship_anytime/aslp_hotword_data/aslp_wake_up_word_data/train.scp", "r")
+    temp = f.read()
+    train_list = temp.split('\n')
+    f.close()
+
+    f = open("/home/disk2/internship_anytime/aslp_hotword_data/aslp_wake_up_word_data/test.scp", "r")
+    temp = f.read()
+    test_list = temp.split('\n')
+    f.close()
+
+    test_examples = []
+    test_labels = []
+    test_length = []
+    test_num = 0
+
+    for example in test_list:
+        if example == '':
+            continue
+        if example.split('/').count("positive")<1:
+            continue
+        start = position_data.find(getname(example))
+        end = position_data.find("positive", start + 1)
+        if end != -1:
+            position_str = position_data[start + 15: end - 1]
+        else:
+            position_str = position_data[start + 15: end]
+
+        # start and end position of "hello" & start and end position of "xiao gua"
+        keyword_position = position_str.split(" ")
+
+        file_path = example.split(" ")[1]
+
+        keyword_frame_position = []
+        for i in range(4):
+            fbank = fbank_reader.HTKFeat_read(file_path).getall()
+            length = fbank.shape[0]
+            frame_position = int(keyword_position[i]) // 160
+            if frame_position >= length:
+                frame_position = length - 1
+            keyword_frame_position.append(frame_position)
+
+        test_length.append(keyword_frame_position[1] - keyword_frame_position[0] + 1 +
+                                    keyword_frame_position[3] - keyword_frame_position[2] + 1)
+
+#        print( example)
+        for frame in range(keyword_frame_position[0], keyword_frame_position[1] + 1):
+            test_examples.append(frame_combine(frame, file_path, keyword_frame_position[0], keyword_frame_position[1]))
+            test_labels.append('0')
+            test_num += 1
+        for frame in range(keyword_frame_position[2], keyword_frame_position[3] + 1):
+            test_examples.append(frame_combine(frame, file_path, keyword_frame_position[2], keyword_frame_position[3]))
+            test_labels.append('1')
+            test_num += 1
+#            print(np.array(test_examples).shape)
+    for example in test_list:
+        if example == '':
+            continue
+        if example.split('/').count("negative")<1:
+            continue
+        file_path=example.split(" ")[1]
+
+        fbank = fbank_reader.HTKFeat_read(file_path).getall()
+        frame_number = fbank.shape[0]
+        test_length.append(frame_number)
+#        print (example)
+        for frame in range(frame_number):
+            test_examples.append(frame_combine(frame, file_path, 0, frame_number - 1))
+            test_labels.append('2')
+            test_num += 1
+    fbank_end_frame = []
+    for i in range(len(test_length)):
+        fbank_end_frame.append(sum(test_length[0: i+1]))
+
+    index = np.arange(len(train_list))
+    np.random.shuffle(index)
+    train_list = np.array(train_list)[index]
+
+    train = TrainSet(train_list, position_data)
+    test = TestSet(test_examples, test_labels, test_num, fbank_end_frame)
+
+    return train, test
---
+++ util.py
@@ -0,0 +1,116 @@
+import math
+import matplotlib.pyplot as plt
+def getMax(smooth_probability,h_max,j):
+    max_label0 = -1
+    max_label1 = -1
+    for i in range(h_max,j+1):
+        # print("cutcutcut",smooth_probability[i][0],smooth_probability[i][1])
+        if smooth_probability[i][0] > max_label0:
+            max_label0 = smooth_probability[i][0]
+        if smooth_probability[i][1] > max_label1:
+            max_label1 = smooth_probability[i][1]
+    return max_label0,max_label1
+
+
+def posteriorHandling(probability,fbankEndFrame):
+    
+
+    confidence = []
+
+    for i in range(len(fbankEndFrame)):
+        if i == 0:
+            fbank_probability = probability[0:fbankEndFrame[0]]
+        else:
+            fbank_probability = probability[fbankEndFrame[i - 1]:fbankEndFrame[i]]
+        
+        #print("fbank_pro",fbank_probability)
+        smooth_probability = []
+
+        for j in range(len(fbank_probability)):
+            sum_label0 = 0
+            sum_label1 = 0
+            sum_label2 = 0
+            w_smooth = 30
+            h_smooth = max(0,(j - w_smooth + 1))
+
+            division = j - h_smooth + 1
+            for temp in range(h_smooth,j+1):
+                sum_label0 += fbank_probability[temp][0]
+                sum_label1 += fbank_probability[temp][1]
+                sum_label2 += fbank_probability[temp][2]
+            smooth_probability.append([sum_label0/division,sum_label1/division,sum_label2/division])
+        
+        #print("smooth",smooth_probability)
+
+        frame_confidence = []
+        confidence_temp = 0
+
+        for j in range(len(fbank_probability)):
+            w_max = 100
+            h_max = max(0,(j - w_max + 1))
+            max_label0,max_label1 = getMax(smooth_probability,h_max,j)
+            confidence_temp = (max_label0 * max_label1)
+            frame_confidence.append(confidence_temp)
+
+        confidence.append(math.sqrt(max(frame_confidence)))
+
+        #print("fbank_pro",fbank_probability)
+        #print("smooth",smooth_probability)
+        #print("frame_confi",frame_confidence)
+        #print("confi",confidence)
+    #print("smoooth_pro",smooth_probability)
+    return confidence
+def plot(false_alarm_rate_list, false_reject_rate_list):
+
+    plt.figure(figsize=(8, 4))
+    plt.plot(false_alarm_rate_list, false_reject_rate_list)
+    plt.xlabel('false_alarm_rate')
+    plt.ylabel('false_reject_rate')
+    plt.title('ROC')
+    plt.savefig("new.png")
+    plt.show()
+def get_label_list():
+    l=[]
+    f = open("/home/disk2/internship_anytime/aslp_hotword_data/aslp_wake_up_word_data/test.scp", "r")
+    temp = f.read()
+    test_list = temp.split('\n')
+    for example in test_list:
+        if example.split('/').count("positive"):
+            l.append(1)
+        else :
+            l.append(0)
+    f.close()
+#    print(l)
+    return l
+def do_eval(confidence):
+    label=get_label_list()
+    threshold_part = 10000
+    false_alarm_rate_list=[]
+    false_reject_rate_list=[]
+
+    for i in range(1,threshold_part):
+        threshold=float(i)/threshold_part
+        true_alarm = true_reject = false_reject = false_alarm = 0
+        for j in range(len(confidence)):
+            if confidence[j]<threshold:
+                if label[j]==0:
+                    true_reject+=1
+                else:
+                    false_reject+=1
+            else :
+                if label[j]==0:
+                    false_alarm+=1
+                else :
+                    true_alarm+=1
+        if false_reject + true_reject == 0 or false_alarm + true_alarm == 0:
+            continue
+        false_alarm_rate = float(false_alarm) / (false_alarm + true_alarm)
+        false_reject_rate = float(false_reject) / (false_reject + true_reject)
+        false_alarm_rate_list.append(false_alarm_rate)
+        false_reject_rate_list.append(false_reject_rate)
+    plot(false_alarm_rate_list, false_reject_rate_list)
+    print(false_alarm_rate_list[0::100])
+
+
+
+
